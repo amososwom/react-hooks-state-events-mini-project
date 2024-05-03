696dbdbd761e6d91e47c0cc730d386257048c1cb
amososwom <amososwom162@gmail.com>
2024-05-03 14:03:47 +0300

    update Code

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,11 +3,13 @@ import React from "react";
 function Task({category, text, deleteTask}) {
   
   return (
+    
     <div className="task">
       <div className="label">{category}</div>
       <div className="text">{text}</div>
       <button className="delete" onClick={()=>deleteTask(text)}>X</button>
     </div>
+
   );
 }
 
